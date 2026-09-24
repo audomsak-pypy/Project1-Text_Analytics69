eed3ef02e78fa2924e989af70013d2ca841fb649
AUdoommsp <audomsak.p@kkumail.com>
2026-09-24 23:10:14 +0700

    update command for toto

--- basic_command.sh
+++ basic_command.sh
@@ -4,3 +4,7 @@
 .\.venv\scripts\Activate.ps1
 ## Project ##
 cd .\Project1-Text_Analytics69
+## First, fetch the latest data from all remote sources.
+git reset --hard origin/toto
+##Force the local file to switch to using the data on origin/toto.
+git reset --hard origin/toto
