e457d6940098e9bd19c17394954e240b5e711937
AUdoommsp <audomsak.p@kkumail.com>
2026-09-24 23:10:58 +0700

    update command for toto again

--- basic_command.sh
+++ basic_command.sh
@@ -5,6 +5,6 @@
 ## Project ##
 cd .\Project1-Text_Analytics69
 ## First, fetch the latest data from all remote sources.
-git reset --hard origin/toto
+git fetch origin
 ##Force the local file to switch to using the data on origin/toto.
 git reset --hard origin/toto
